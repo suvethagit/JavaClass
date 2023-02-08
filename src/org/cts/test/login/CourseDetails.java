09abff807a1573de7ebe035813f3c7dc4eb36ea0
suvethagit <suvethapuru97@gmail.com>
2023-02-08 18:37:31 +0530

    new code

---
+++ src/org/cts/test/login/CourseDetails.java
@@ -0,0 +1,18 @@
+package org.cts.test.login;
+
+public class CourseDetails {
+
+	private void javaCourse() {
+		// TODO Auto-generated method stub
+System.out.println("python course is started");
+	}
+	private void pythonCourse() {
+		// TODO Auto-generated method stub
+System.out.println("python course is not started");
+	}
+	public static void main(String[] args) {
+		CourseDetails c= new CourseDetails();
+		c.javaCourse();
+		c.pythonCourse();
+	}
+}
